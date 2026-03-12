13b7e22df4f5b57a7d90a403d73bdd0abfcd23a4
Omar Romera <oromeraa@uoc.edu>
2026-03-12 02:24:21 +0100

    logout hecho

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit {
   constructor(
     private router: Router,
     private headerMenusService: HeaderMenusService,
-    private localStorageService: LocalStorageService
+    private localStorageService: LocalStorageService,
   ) {
     this.showAuthSection = false;
     this.showNoAuthSection = true;
@@ -29,7 +29,7 @@ export class HeaderComponent implements OnInit {
           this.showAuthSection = headerInfo.showAuthSection;
           this.showNoAuthSection = headerInfo.showNoAuthSection;
         }
-      }
+      },
     );
   }
 
@@ -58,6 +58,13 @@ export class HeaderComponent implements OnInit {
   }
 
   logout(): void {
-    // TODO 15
+    this.localStorageService.remove('user_id');
+    this.localStorageService.remove('access_token');
+    const headerInfo: HeaderMenus = {
+      showAuthSection: false,
+      showNoAuthSection: true,
+    };
+    this.headerMenusService.headerManagement.next(headerInfo);
+    this.router.navigateByUrl('home');
   }
 }
