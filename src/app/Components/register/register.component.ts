ec5707daa3e3123a0a1ca7fe087d2047f7846709
Omar Romera <oromeraa@uoc.edu>
2026-03-12 02:02:01 +0100

    Registro hecho

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -5,14 +5,32 @@ import { HeaderMenusService } from 'src/app/Services/header-menus.service';
 import { SharedService } from 'src/app/Services/shared.service';
 import { UserService } from 'src/app/Services/user.service';
 
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { UserDTO } from 'src/app/Models/user.dto';
+import { formatDate } from '@angular/common';
+import { HeaderMenus } from 'src/app/Models/header-menus.dto';
+
+type RegisterFormModel = {
+  name: FormControl<string>;
+  surname_1: FormControl<string>;
+  surname_2: FormControl<string>;
+  alias: FormControl<string>;
+  birth_date: FormControl<string>;
+  email: FormControl<string>;
+  password: FormControl<string>;
+};
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  /*
-  // TODO: implementar tipo RegisterFormModel
+  // TODO: implementar tipo RegisterFormModel 
 
   // TODO 16
   registerUser: UserDTO;
@@ -27,7 +45,8 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup<RegisterFormModel>;
   isValidForm: boolean | null;
-*/
+
+
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
@@ -36,12 +55,60 @@ export class RegisterComponent implements OnInit {
     private router: Router,
   ) {
     // TODO 17
+    this.registerUser = new UserDTO('', '', '', '', new Date(), '', '');
+    this.isValidForm = null;
+
+
+    this.name = this.formBuilder.control('', {
+      validators: [Validators.required, Validators.minLength(4), Validators.maxLength(25)],
+      nonNullable: true,
+    });
+
+    this.surname_1 = new FormControl('', {
+      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(25)],
+      nonNullable: true,
+    });
+
+    this.surname_2 = new FormControl('', {
+      validators: [Validators.minLength(5), Validators.maxLength(25)],
+      nonNullable: true,
+    });
+
+    this.alias = new FormControl('', {
+      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(25)],
+      nonNullable: true,
+    });
+
+    this.birth_date = new FormControl(formatDate(new Date(), 'yyyy-MM-dd', 'en'), {
+      validators: [Validators.required],
+      nonNullable: true,
+    });
+
+    this.email = new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    });
+
+    this.password = new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8), Validators.maxLength(16)],
+      nonNullable: true,
+    });
+
+    this.registerForm = this.formBuilder.group({
+      name: this.name,
+      surname_1: this.surname_1,
+      surname_2: this.surname_2,
+      alias: this.alias,
+      birth_date: this.birth_date,
+      email: this.email,
+      password: this.password,
+    });
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   async register(): Promise<void> {
-    /*
     let responseOK = false;
     this.isValidForm = false;
     let errorResponse: any;
@@ -100,6 +167,5 @@ export class RegisterComponent implements OnInit {
 
       this.router.navigateByUrl('home');
     }
-    */
   }
 }
